ba46ea34f3fde18cc1e369cc53a374ffa33e9c40
senthil <Thyagarajan.Senthil@bcg.com>
2022-10-13 11:59:15 +0530

    added api route for app 1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 5000;
 
 let baseurl = "/api"
 //Idiomatic expression in express to route and respond to a client request
-app.get(baseurl + '/', (req, res) => {        //get requests to the root ("/") will route here
+app.get('/', (req, res) => {        //get requests to the root ("/") will route here
     res.json({ 'Name': "Hello" })      //server responds by sending the index.html file to the client's browser
     //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
